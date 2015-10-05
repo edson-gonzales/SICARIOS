a557ab68545a4adbecca637a8f5706b897ed3d7b
royyery <royyery@hotmail.com>
2015-10-05 00:44:29 -0400

    save method for membership class al its test, customer and its test and finally the rent class

---
+++ src/admin_module/user_module/membership.py
@@ -0,0 +1,73 @@
+__author__ = 'Roy Ortiz'
+
+import datetime
+from datetime import datetime
+from db.transactions.DBManager import DBManager
+
+
+class Membership(object):
+    def __init__(self, type, percent_of_discount, start_date):
+        """
+        type is a string it stores the membership type
+        percent_of_discount is an int value it will store the percentage of discount from 0 to 100
+        start_date is a date value to indicate the membership start date
+        """
+        self._type = type
+        self._percent_of_discount = percent_of_discount
+        self._start_date = start_date
+
+    def get_type(self):
+        """
+        Returns the type of membership
+        :return: the type name
+        """
+        return self._type
+
+    def get_discount(self, membership_type):
+        """
+        Returns the percentage of discount according the membership type
+        method receives the membership type to search the percent discount according membership
+        :return: the percentage from 0 to 100
+        """
+        conn = DBManager()
+        discount = 0
+        execution_query = "select percent_discount from membership where type='" + membership_type + "'"
+        try:
+            discount = conn.query(execution_query).fetchone()[0]
+        except ValueError:
+            print ("there was an error in the process")
+        return discount
+
+    def get_start_date(self):
+        """
+        Returns the membership start date
+        :return: the suscription start date
+        """
+        return self._start_date
+
+    def calculate_end_date(self, start_date):
+        """
+        Returns the end date calculated from start date this by adding 6 months
+        :return: the membership end date
+        """
+        end_date = start_date + datetime.timedelta(6*365/12)
+        return end_date
+
+    def save_membership(self):
+        """
+        insert the new membership to database
+        :return:
+        """
+        conn = DBManager()
+        type = self.get_type()
+        percent = str(self._percent_of_discount)
+        execution_query = "insert into membership values('" + type + "'," + percent + ")"
+        try:
+            conn.query(execution_query)
+        except ValueError:
+            print ("there was an error in the process")
+
+
+
+
+
---
+++ test/test_membership.py
@@ -0,0 +1,49 @@
+__author__ = 'Roy Ortiz'
+
+import unittest
+from admin_module.user_module.membership import Membership
+
+
+class CustomerTest(unittest.TestCase):
+
+    def test_create_a_new_membership_instance(self):
+        """
+        this will verify that a new membership instance is created
+        :return:
+        """
+        membership_type = "gold"
+        percent_discount = 50
+        start_date = 10/10/2015
+        new_membership = Membership(membership_type, percent_discount, start_date)
+        self.assertTrue(isinstance(new_membership, Membership))
+
+    def test_type_value_saved_for_membership(self):
+        """
+        this will verify that a the membership type is saved and persist
+        """
+        membership_type = "gold"
+        percent_discount = 50
+        start_date = 10/10/2015
+        new_membership = Membership(membership_type, percent_discount, start_date)
+        self.assertEqual(new_membership.get_type(), membership_type)
+
+    def test_percent_discount_value_saved_for_membership(self):
+        """
+        this will verify that a the membership percent discount is saved and persist
+        """
+        membership_type = "gold"
+        percent_discount = 50
+        start_date = 10/10/2015
+        new_membership = Membership(membership_type, percent_discount, start_date)
+        self.assertEqual(new_membership.get_discount(membership_type), percent_discount)
+
+    def test_membership_is_saved_in_database(self):
+        """
+        this will verify that the membership values are saved on db as expected
+        """
+        membership_type = "blue"
+        percent_discount = 40
+        start_date = 10/10/2015
+        new_membership = Membership(membership_type, percent_discount, start_date)
+        new_membership.save_membership()
+        self.assertEqual(new_membership.get_discount(membership_type), percent_discount)
